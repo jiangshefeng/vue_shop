6cd90cc14cedcd8839df16518bdd7373c8557a7f
姜滠峰 <1104233665@qq.com>
2021-03-29 14:29:36 +0800

    '完成'

--- src/main-dev.js
+++ src/main-dev.js
@@ -6,7 +6,10 @@ import './assets/css/global.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
 import VueQuillEditor from 'vue-quill-editor'
+import nprogress from 'nprogress'
 
+// 导入nprogress包对应的css
+import 'nprogress/nprogress.css'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
@@ -14,11 +17,20 @@ import 'quill/dist/quill.bubble.css' // for bubble theme
 Vue.use(VueQuillEditor)
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request拦截器中展示进度条
 axios.interceptors.request.use(config => {
+  nprogress.start()
   config.headers.Authorization = sessionStorage.getItem('token')
 
   return config
 })
+// 在response拦截器中隐藏进度条
+axios.interceptors.response.use(
+  config => {
+    nprogress.done()
+    return config
+  }
+)
 Vue.filter('dateFormat', function (val) {
   const dt = new Date(val)
   const Y = dt.getFullYear()
--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,10 @@
-module.exports = {}
+module.exports = {
+  chainWebpack: config => {
+    config.when(process.env.NODE_ENV === 'production', config => {
+      config.entry('app').clear().add('./src/main-prod.js')
+    })
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.entry('app').clear().add('./src/main-dev.js')
+    })
+  }
+}
